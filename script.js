c20f7046975a0a08dd5c8fea756e0708bbdeb86a
amang10 <90174464+amang10@users.noreply.github.com>
2025-04-25 09:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function () {
+  return ${this.make} ${this.model};
+};
+
+function SportsCar(make, model, topSpeed) {
+  // Inherit properties from Car
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit methods from Car's prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed method specific to SportsCar
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
